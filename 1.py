956dc2e780e6a526d42ff79c93165f50775bd8c9
Chandler Chen <hongqche@cisco.com>
2021-12-06 13:42:07 +0800

    puzzle 1_2

--- 1.py
+++ 1.py
@@ -2,13 +2,25 @@ from utils import *
 
 lines = read_lines("input.txt")
 
+# part 1
 res = 0
 current = int(lines[0])
 for l in lines[1:]:
     i = int(l)
     if i > current:
-        res+=1
+        res += 1
     current = i
+print("part 1 answer: " + str(res))
 
-print(res)
+# part 2
+res = 0
+for i, l in enumerate(lines[0:-2]):
+    if i == 0:
+        current = int(lines[i]) + int(lines[i+1]) + int(lines[i+2])
+        continue
+    sum = int(lines[i]) + int(lines[i+1]) + int(lines[i+2])
+    if sum > current:
+        res += 1
+    current = sum
 
+print("part 2 answer: " + str(res))
